60f762f4d4116af4193fe7a81eeeadc992d4bbf1
blacdev <stevenowoborode@gmail.com>
2021-08-19 08:47:11 +0100

    image change

--- config/settings.py
+++ config/settings.py
@@ -130,10 +130,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 # Email backend config
-EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
-
-EMAIL_PORT = os.getenv("EMAIL_PORT")
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+# EMAIL_HOST = os.getenv("EMAIL_HOST")
+# EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
+#
+# EMAIL_PORT = os.getenv("EMAIL_PORT")
+# EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 django_heroku.settings(locals())
